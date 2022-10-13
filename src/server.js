d9d18ccecbf0b06c200815d82948741fdb92cd94
DevZsaosao <huyvu10012003@gmail.com>
2022-10-13 14:08:48 +0700

    second

--- src/server.js
+++ src/server.js
@@ -2,16 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import viewEngine from './config/viewEngine';
 import initWebRoutes from './route/wed';
+import connectDB from './config/connectDB';
 require('dotenv').config();
 let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 viewEngine(app);
 initWebRoutes(app);
-
+connectDB();
 let port = process.env.PORT || 6969;
 // PORT === undefined => POTR = 6969
 
 app.listen(port, () => {
-    console.log(`Backend NOdeJS is running on the port : ${port}`);
+    console.log(`Backend NodeJS is running on the port : ${port}`);
 });
